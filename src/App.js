e3561b0fda3e6b37a0c40f160118d3dec8185e1d
shahriage <riage@bs-23.net>
2022-03-13 18:46:50 +0600

    solve cross origin issue

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React, {useState, useEffect} from 'react';
-import axios from 'axios';
-import './App.scss';
-import Header from './components/Header/Header';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.scss";
+import Header from "./components/Header/Header";
 
 function App(props) {
   let count = 0;
@@ -11,49 +11,71 @@ function App(props) {
 
   useEffect(() => {
     axios
-      .get(`https://www.kayak.com/h/mobileapis/directory/airlines/homework`)
-      .then(res => {
+      .get(`h/mobileapis/directory/airlines/homework`)
+      .then((res) => {
         const dataItems = res.data;
-        const header_refactor = dataItems.replace('/**/invalidCallbackFunctionName(','');
-        const footer_refactor = header_refactor.replace(');','');
+        const header_refactor = dataItems.replace(
+          "/**/invalidCallbackFunctionName(",
+          ""
+        );
+        const footer_refactor = header_refactor.replace(");", "");
         const ItemsData = JSON.parse(footer_refactor);
-        setItems(ItemsData)
+        setItems(ItemsData);
       })
-      .catch((error) => console.log('error', error));
-  }, [])
+      .catch((error) => console.log("error", error));
+  }, []);
 
   return (
     <div className="App">
-
       {/* Header START */}
-      <Header/>
+      <Header />
       {/* Header END */}
 
       <main>
-        <div className='tile'>
-          <div className='tile__wrapper'>
-            <h1 className='tile__title'>Airline</h1>
+        <div className="tile">
+          <div className="tile__wrapper">
+            <h1 className="tile__title">Airline</h1>
 
             {/* Filter START */}
-            <div className='tile__filter'>
-              <p><strong>Filter by Alliances</strong></p>
+            <div className="tile__filter">
+              <p>
+                <strong>Filter by Alliances</strong>
+              </p>
 
-              <div className='tile__filter__group'>
-                <div className='tile__filter__group-item'>
-                  <label className="checkbox" onChange={event => {setAlliance(event.target.value)}}>Oneworld
-                    <input value="OW" name="tile__filter" type="radio"/>
+              <div className="tile__filter__group">
+                <div className="tile__filter__group-item">
+                  <label
+                    className="checkbox"
+                    onChange={(event) => {
+                      setAlliance(event.target.value);
+                    }}
+                  >
+                    Oneworld
+                    <input value="OW" name="tile__filter" type="radio" />
                     <span className="checkmark"></span>
                   </label>
                 </div>
-                <div className='tile__filter__group-item'>
-                  <label className="checkbox" onChange={event => {setAlliance(event.target.value)}}>Sky Team
-                    <input value="ST" name="tile__filter" type="radio"/>
+                <div className="tile__filter__group-item">
+                  <label
+                    className="checkbox"
+                    onChange={(event) => {
+                      setAlliance(event.target.value);
+                    }}
+                  >
+                    Sky Team
+                    <input value="ST" name="tile__filter" type="radio" />
                     <span className="checkmark"></span>
                   </label>
                 </div>
-                <div className='tile__filter__group-item'>
-                  <label className="checkbox" onChange={event => {setAlliance(event.target.value)}}>Star Alliance
-                    <input value="SA" name="tile__filter" type="radio"/>
+                <div className="tile__filter__group-item">
+                  <label
+                    className="checkbox"
+                    onChange={(event) => {
+                      setAlliance(event.target.value);
+                    }}
+                  >
+                    Star Alliance
+                    <input value="SA" name="tile__filter" type="radio" />
                     <span className="checkmark"></span>
                   </label>
                 </div>
@@ -62,59 +84,88 @@ function App(props) {
             {/* Filter END */}
 
             {/* Airline Itemlist START */}
-            <div className={`tile__body ${expand == false ? 'toggle-expand' : ''}`}>
-              <ul className='tile__list-items'>
-                {alliance == '' ?
-                  items.map(elem => {
-                    const {name, phone, site, logoURL, alliance} = elem;
-                    count++;
-                    return(
-                      <li id={count} key={count}>
-                        <a href="#">
-                          <div className='tile__list-items__logo'>
-                            <img src={'https://www.kayak.com' + logoURL} alt="Logo"/>
-                          </div>
-                          <div className='tile__list-items__details'>
-                            <p className='airline-name'>{name}</p>
-                            <div className='hidden-info'>
-                              <p>{alliance === 'SA' ? 'Star Alliance' : alliance === 'OW' ? 'One World' : alliance === 'ST' ? 'Sky Team' : ''}</p>
-                              <p>{phone}</p>
-                              <p className='web-link'>{site}</p>
+            <div
+              className={`tile__body ${expand === false ? "toggle-expand" : ""}`}
+            >
+              <ul className="tile__list-items">
+                {alliance == ""
+                  ? items.map((elem) => {
+                      const { name, phone, site, logoURL, alliance } = elem;
+                      count++;
+                      return (
+                        <li id={count} key={count}>
+                          <a href={site}>
+                            <div className="tile__list-items__logo">
+                              <img
+                                src={"https://www.kayak.com" + logoURL}
+                                alt="Logo"
+                              />
                             </div>
-                          </div>
-                        </a>
-                      </li>
-                    )  
-                  }) :
-                  items.filter(elem => elem.alliance === alliance).map(elem => {
-                    const {name, phone, site, logoURL, alliance} = elem;
-                    count++;
-                    return(
-                      <li id={count} key={count}>
-                        <a href="#">
-                          <div className='tile__list-items__logo'>
-                            <img src={'https://www.kayak.com' + logoURL} alt="Logo"/>
-                          </div>
-                          <div className='tile__list-items__details'>
-                            <p className='airline-name'>{name}</p>
-                            <div className='hidden-info'>
-                              <p>{alliance === 'SA' ? 'Star Alliance' : alliance === 'OW' ? 'One World' : alliance === 'ST' ? 'Sky Team' : ''}</p>
-                              <p>{phone}</p>
-                              <p className='web-link'>{site}</p>
+                            <div className="tile__list-items__details">
+                              <p className="airline-name">{name}</p>
+                              <div className="hidden-info">
+                                <p>
+                                  {alliance === "SA"
+                                    ? "Star Alliance"
+                                    : alliance === "OW"
+                                    ? "One World"
+                                    : alliance === "ST"
+                                    ? "Sky Team"
+                                    : ""}
+                                </p>
+                                <p>{phone}</p>
+                                <p className="web-link">{site}</p>
+                              </div>
                             </div>
-                          </div>
-                        </a>
-                      </li>
-                    )  
-                  }) 
-                }
+                          </a>
+                        </li>
+                      );
+                    })
+                  : items
+                      .filter((elem) => elem.alliance === alliance)
+                      .map((elem) => {
+                        const { name, phone, site, logoURL, alliance } = elem;
+                        count++;
+                        return (
+                          <li id={count} key={count}>
+                            <a href={site}>
+                              <div className="tile__list-items__logo">
+                                <img
+                                  src={"https://www.kayak.com" + logoURL}
+                                  alt="Logo"
+                                />
+                              </div>
+                              <div className="tile__list-items__details">
+                                <p className="airline-name">{name}</p>
+                                <div className="hidden-info">
+                                  <p>
+                                    {alliance === "SA"
+                                      ? "Star Alliance"
+                                      : alliance === "OW"
+                                      ? "One World"
+                                      : alliance === "ST"
+                                      ? "Sky Team"
+                                      : ""}
+                                  </p>
+                                  <p>{phone}</p>
+                                  <p className="web-link">{site}</p>
+                                </div>
+                              </div>
+                            </a>
+                          </li>
+                        );
+                      })}
               </ul>
             </div>
             {/* Airline Itemlist END */}
-            <div className='text-center'>
-              {expand === true ? '' :
-                <button className='show-more' onClick={() => setExpand(true)}>Show more</button>
-              }
+            <div className="text-center">
+              {expand === true ? (
+                ""
+              ) : (
+                <button className="show-more" onClick={() => setExpand(true)}>
+                  Show more
+                </button>
+              )}
             </div>
           </div>
         </div>
